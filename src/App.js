b58b5395a7ef3381e6ded42af98f4cbdd0ad5310
ran <ranerlich@gmail.com>
2023-09-12 18:32:58 +0300

    added state for products and display first product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
   function getProducts() {
     axios
       .get("https://django-rest-product.onrender.com/product")
       .then((response) => {
         console.log(response.data);
+        setProducts(response.data);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -13,6 +17,8 @@ function App() {
   }
   return (
     <>
+      {products.length > 0 ? products[0].name : "no products yet"}
+      <br />
       <button onClick={getProducts}>Get Products</button>
     </>
   );
