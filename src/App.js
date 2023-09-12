229f1417b0680807bbe14f046b704356f879e548
ran <ranerlich@gmail.com>
2023-09-12 17:55:27 +0300

    get products and print to console

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
 
 function App() {
+  function getProducts() {
+    axios
+      .get("https://django-rest-product.onrender.com/product")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={getProducts}>Get Products</button>
+    </>
   );
 }
 
