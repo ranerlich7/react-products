74c4b01bd45fb299ea0e8d80fd9c922bdd34c543
ran <ranerlich@gmail.com>
2023-09-22 11:51:24 +0300

    added files to prev commit

---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <>
+      <div className="container">
+        <footer className="py-3 my-4">
+          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+            <li className="nav-item">
+              <Link to="/" className="nav-link px-2 text-muted">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link px-2 text-muted">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/register" className="nav-link px-2 text-muted">
+                Register
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a
+                href="https://www.google.com"
+                className="nav-link px-2 text-muted"
+              >
+                Google
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/" className="nav-link px-2 text-muted">
+                About
+              </a>
+            </li>
+          </ul>
+          <p className="text-center text-muted">© 2022 Company, Inc</p>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/components/Register.js
@@ -0,0 +1,5 @@
+function Register() {
+  return <>Please Register... Todo: add register here...</>;
+}
+
+export default Register;
