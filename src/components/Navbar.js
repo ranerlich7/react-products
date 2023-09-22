2de028aa42e05ddbee0a943b225ad749351ecab3
ran <ranerlich@gmail.com>
2023-09-22 09:25:49 +0300

    search step 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,35 @@
 import { Link } from "react-router-dom";
 
 function Navbar({ categories, clickButton }) {
+  function searchProduct() {
+    console.log("searching for product");
+  }
   return (
     <>
       <ul className="nav">
+        <li className="nav-item">
+          <button className="nav-link" onClick={() => clickButton("")}>
+            All Products
+          </button>
+        </li>
         {categories.map((category) => (
-          <li className="nav-item">
+          <li key={category.id} className="nav-item">
             <button
               className="nav-link"
-              aria-current="page"
               onClick={() => clickButton(category.id)}
             >
               {category.name} -({category.id})
             </button>
           </li>
         ))}
+        <li className="nav-item">
+          <input />
+        </li>
+        <li className="nav-item">
+          <button className="btn btn-info" onClick={searchProduct}>
+            Search
+          </button>
+        </li>
         <li className="nav-item">
           <Link to="/login">Login</Link>
         </li>
